0366fc725a3770404339311e3973e1fbb22e13bd
Kevin <65086673+RazerKev@users.noreply.github.com>
2022-04-13 17:26:09 -0400

    Modified sample_state.m file. Change from round function to rand function.

--- sample_state.m
+++ sample_state.m
@@ -3,12 +3,11 @@
 %sample of the state that the agent is in.
 function sample_state(v_bins, p_bins, t_bins) 
    
-    %Round the element passed in the paramter, to 
-    % the nearest integer. Make sure any elements passed in are rounded to
-    % whole numbers.
-    V_Sample = round(v_bins); 
-    P_Sample = round(p_bins);
-    T_Sample = round(t_bins);
+    %Grabs the random element in the array for each vector (vector =
+    %sample) and assigned them to each variable.
+    V_Sample = rand(v_bins); 
+    P_Sample = rand(p_bins);
+    T_Sample = rand(t_bins);
 
     Sample = cat(2, V_Sample,P_Sample,T_Sample); %cat function conccatenate 
     % the three vectors along dimension "dim" which is either 1 or 2. 1
