534d5ff18e668aae6b68271d50d55cc3bbaef4c4
Kevin <65086673+RazerKev@users.noreply.github.com>
2022-04-19 11:12:56 -0400

    Updated sample_state
    Sample_state is updated and also uploaded as a separate file.

--- q_algorithim.m
+++ q_algorithim.m
@@ -50,11 +50,20 @@ indx = where(v==-1,p==0,t==0);
 end
 
 %% qwe
-function sampling_states= sample_state(vbins,p-bins,t_bins)
-v_sample = rand(v_bins);
-t_sample = rand(t_bins);
-p_sample = rand(p_bins);
-sample = concat(v_sample,t_sample,p_sample); 
+%Function sample_state grabs the sample of each vector passed in and find
+%the state and concatenate three vectors, into one vector which is one a
+%sample of the state that the agent is in.
+function sample_state(v_bins, p_bins, t_bins) 
+   
+    %Grabs the random element in the array for each vector (vector =
+    %sample) and assigned them to each variable.
+    V_Sample = rand(v_bins); 
+    P_Sample = rand(p_bins);
+    T_Sample = rand(t_bins);
+
+    Sample = cat(2, V_Sample,P_Sample,T_Sample); %cat function conccatenate 
+    % the three vectors along dimension "dim" which is either 1 or 2. 1
+    % concatenates vertically while 2 concatenates horizontally.
 end
 %% qwe 
 function binned_state = binning(cont_state)
